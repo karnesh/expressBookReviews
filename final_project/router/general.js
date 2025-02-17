6f5740117a49de27257b608350d3b676e47172cf
karnesh <jain.karnesh@gmail.com>
2025-02-16 23:07:11 -0500

    implemented method to get books based on isbn and author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -22,14 +22,45 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  try{
+    const isbn = req.params.isbn;
+    const book = books[isbn];
+    if(book){
+      res.json(book)
+    } else{
+      res.status(404).json({message: "book not found"});
+    }
+  }catch {
+    console.error(error);
+    res.status(500).json({message: "Error retrieving book details based on ISBN"});
+  }
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  try{
+    const author = req.params.author;
+    const booklist = [];
+
+    const bookKeys = Object.keys(books);
+
+    // Iterate through books and find matches
+    for (const key of bookKeys) {
+      const book = books[key];
+      if (book.author === author) {
+        booklist.push(book);
+      }
+    }
+
+    if (booklist.length > 0) {
+      res.json(booklist); // Send matching books as a JSON response
+    } else {
+      res.status(404).json({ message: "No books found by that author" }); // Handle no books found
+    }
+  }catch{
+    console.error(error);
+    res.status(500).json({message: "Error getting book details based on author"})
+  }
 });
 
 // Get all books based on title
